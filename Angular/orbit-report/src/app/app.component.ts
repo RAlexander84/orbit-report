ab92c7803596b82bc41ead668877a34cc70f38ca
Riley Alexander <riley.alexander@me.com>
2021-01-07 00:03:08 -0600

    update

--- Angular/orbit-report/src/app/app.component.ts
+++ Angular/orbit-report/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SatelliteComponent } from './satellite/satellite.component'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+  sourceList: SatelliteComponent[];
+  constructor() {
+    this.sourceList = [
+       new SatelliteComponent("SiriusXM", "Communication", "2009-03-21", "LOW", true),
+       new SatelliteComponent("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
+       new SatelliteComponent("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
+       new SatelliteComponent("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
+       new SatelliteComponent("ISS", "Space Station", "1998-11-20", "LOW", true),
+    ];
+  }
 }
--- Angular/orbit-report/src/app/app.module.ts
+++ Angular/orbit-report/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SatelliteComponent } from './satellite/satellite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SatelliteComponent
   ],
   imports: [
     BrowserModule,
