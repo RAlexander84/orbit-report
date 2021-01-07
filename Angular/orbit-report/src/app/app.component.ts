89af40bfb068226c15ae3cfcfd89a8a833153b96
Riley Alexander <riley.alexander@me.com>
2021-01-07 15:53:32 -0600

    tons

--- Angular/orbit-report/src/app/app.component.ts
+++ Angular/orbit-report/src/app/app.component.ts
@@ -1,21 +1,46 @@
-import { Component } from '@angular/core';
-import { SatelliteComponent } from './satellite/satellite.component'
+import { Component, OnInit } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
-  selector: 'app-root',
+  selector: 'AppComponent',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent  implements OnInit {
   title = 'orbit-report';
-  sourceList: SatelliteComponent[];
+  sourceList: Satellite[];
+  displayList: Satellite[];  
   constructor() {
-    this.sourceList = [
-       new SatelliteComponent("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-       new SatelliteComponent("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-       new SatelliteComponent("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-       new SatelliteComponent("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-       new SatelliteComponent("ISS", "Space Station", "1998-11-20", "LOW", true),
-    ];
+    this.displayList = [];
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+        let fetchedSatellites = data.satellites;
+        for (let i = 0; i < fetchedSatellites.length; i++) {
+          let newSat = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+          this.sourceList.push(newSat);
+        }
+        this.displayList = this.sourceList.slice(0);
+      }.bind(this));
+    }.bind(this));
+ 
+  }
+
+  ngOnInit(): void{
+  }
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+      let name = this.sourceList[i].name.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+      }
+    }
+    this.displayList = matchingSatellites;
   }
 }
--- Angular/orbit-report/src/app/app.module.ts
+++ Angular/orbit-report/src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SatelliteComponent } from './satellite/satellite.component';
+import { OrbitListComponent } from './orbit-list/orbit-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SatelliteComponent
+    OrbitListComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
