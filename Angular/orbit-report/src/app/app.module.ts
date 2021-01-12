882b718d4d1b33854817c5019f76bcacd8797d67
Riley Alexander <riley.alexander@me.com>
2021-01-12 13:19:18 -0600

    new

--- Angular/orbit-report/src/app/app.module.ts
+++ Angular/orbit-report/src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OrbitListComponent } from './orbit-list/orbit-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    OrbitListComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
